3945c4b071598d146980910a0f22c05a55dc091f
AndrewGrishchenko <andrew26080537@gmail.com>
2025-05-06 13:16:43 +0300

    Added main.cpp logic added checking for file and specifying file name

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "ClubManager.h"
 
-int main() {
-    ClubManager* manager = new ClubManager("input.txt");
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "No file specified. Exiting..." << std::endl;
+        return 1;
+    }
+
+    ClubManager* manager = new ClubManager(argv[1]);
     if (!manager->process()) {
         std::cout << "Error has occured; program stopped" << std::endl;
         return 1;
